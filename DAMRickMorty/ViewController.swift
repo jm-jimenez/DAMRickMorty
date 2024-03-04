d51832b8d2f5d717458fc41b4384d067ace7419d
José María Jiménez <josemariajimenezperez@gmail.com>
2024-03-04 12:19:02 +0100

    Initial Commit

---
+++ DAMRickMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DAMRickMorty
+//
+//  Created by José María Jiménez on 4/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
